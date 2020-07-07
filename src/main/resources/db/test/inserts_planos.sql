5a963e7781d8a7ea912f896a7492e9d651ae2d23
arthur lima <arthur@172.20.80.74>
2020-07-06 23:10:12 -0300

    Inserçao de dados de para testes e utilizacao da API

---
+++ src/main/resources/db/test/inserts_planos.sql
@@ -0,0 +1,3 @@
+insert into plano(id,franquia,nome,obs) values (2,30,'FaleMais30','');
+--insert into plano(id,franquia,nome,obs) values (2,60,'FaleMais60','');
+--insert into plano(id,franquia,nome,obs) values (3,120,'FaleMais120','');
